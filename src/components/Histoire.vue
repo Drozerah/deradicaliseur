8f38995eaf9275a0afed871876035f5ac7fb6396
Drozerah <drozerah@drozerah.user>
2018-10-16 20:20:34 +0200

    feat: add Histoire.vue component

---
+++ src/components/Histoire.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="histoire">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Histoire',
+  data(){
+    return{
+      title: "Histoire component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
