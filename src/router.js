c6f32cf9d960ab52935bf955a85f909900f73ae3
Drozerah <drozerah@drozerah.user>
2018-10-16 20:23:51 +0200

    feat: add path /histoire to router.js

--- src/router.js
+++ src/router.js
@@ -2,15 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './components/Home.vue'
+import Histoire from './components/Histoire.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home.vue
+        path: '/',
+        name: 'home',
+        component: Home.vue
+    },
+    {
+        path: '/histoire',
+        name: 'histoire',
+        component: Histoire.vue
     }
   ]
 })
