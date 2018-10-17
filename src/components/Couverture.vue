5e1ee79a61e2ec1d331f9f430bb14d37d1333c8c
Drozerah <drozerah@drozerah.user>
2018-10-17 11:08:18 +0200

    feat: add Couverture.vue component

---
+++ src/components/Couverture.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="couverture">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Couverture',
+  data(){
+    return{
+      title: "Couverture component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
