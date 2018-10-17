04d596ddae67ddb6293b1fd2f277b5593afebb2d
Drozerah <drozerah@drozerah.user>
2018-10-17 15:58:32 +0200

    feat: add Plat_verso.vue component

---
+++ src/components/Plat_verso.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="plat_verso">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Plat_verso',
+  data(){
+    return{
+      title: "Plat verso component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
