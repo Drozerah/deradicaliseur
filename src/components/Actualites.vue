6da7d821d333d9a8316ceb524488335bd24616c6
Drozerah <drozerah@drozerah.user>
2018-10-17 15:30:25 +0200

    feat: add Actualites.vue component

---
+++ src/components/Actualites.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="actualites">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Actualites',
+  data(){
+    return{
+      title: "Actualités component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
