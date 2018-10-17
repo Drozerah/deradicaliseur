123a35b121dc4986ee8d1c7ef604853c3dbc7c60
Drozerah <drozerah@drozerah.user>
2018-10-17 15:38:49 +0200

    feat: add Auteurs.vue component

---
+++ src/components/Auteurs.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="auteurs">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Auteurs',
+  data(){
+    return{
+      title: "Auteurs component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
