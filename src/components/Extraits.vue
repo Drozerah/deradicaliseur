ab4b6edab69e72446ee95c72dcffec2eebd2c493
Drozerah <drozerah@drozerah.user>
2018-10-17 15:22:17 +0200

    feat: add Extraits.vue component

---
+++ src/components/Extraits.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="extraits">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Extraits',
+  data(){
+    return{
+      title: "Extraits component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
