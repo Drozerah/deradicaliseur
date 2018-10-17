27afd15ae77d1bb6d2898c66b93eee897bfc7661
Drozerah <drozerah@drozerah.user>
2018-10-17 10:08:33 +0200

    feat: add Dispo.vue component

---
+++ src/components/Dispo.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="disponible">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Disponible',
+  data(){
+    return{
+      title: "Disponibilités component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
