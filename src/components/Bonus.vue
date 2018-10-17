4eaff566fd8d729455e954760d9bd45402617354
Drozerah <drozerah@drozerah.user>
2018-10-17 15:45:53 +0200

    feat: add Bonus.vue component

---
+++ src/components/Bonus.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="bonus">
+    <header class="article-header">
+      <h1 class="article-title">{{ title }}</h1>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Bonus',
+  data(){
+    return{
+      title: "Bonus component title"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
