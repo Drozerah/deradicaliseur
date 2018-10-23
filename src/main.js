dc9153ab30e603c00c7f8ad3b42444ff17a33a3f
Drozerah <drozerah@drozerah.user>
2018-10-24 00:13:22 +0200

    feat: import materialize.min.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'materialize-css/dist/css/materialize.min.css'
 
 Vue.config.productionTip = false
 
-new Vue({
+// eslint-disable-next-line
+let vm = new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
