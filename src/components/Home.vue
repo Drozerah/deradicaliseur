2e75e1b1b1975227d962815bc67eaa0a76c675ff
Drozerah <drozerah@drozerah.user>
2018-10-24 16:40:10 +0200

    style: fix CSS font-size

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <article class="home">
-    <header class="article-header">
+    <header class="article-header row">
       <h1 class="article-title" v-show="isTitle">{{ title }}</h1>
-      <div class="flex-container">
+      <div class="flex-container s12 m12 l12">
         <div class="flex-item"><img class="responsive-img" alt="Le déradicaliseur image BD" src="../assets/le_deradicaliseur_p.jpg"></div>
         <p class="flex-item">
           Lui, <br>
@@ -32,8 +32,10 @@
 <style scoped>
 
 header p {
-  font-size: 22px;
+  font-size: 20px;
+  /* font-size: 2vw; */
   text-align: left;
+  /* font-weight: 400; */
 }
 
 .flex-container{
