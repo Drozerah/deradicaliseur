fcd9f0d5bba6e6615ebc1f8254e2af27a72a044f
Drozerah <drozerah@drozerah.user>
2018-10-26 01:46:56 +0200

    style: formating

--- src/components/tools/CarouselExtraits.vue
+++ src/components/tools/CarouselExtraits.vue
@@ -1,147 +1,150 @@
 <template>
-    <div class="row">  
+    <div class="row">
         <div class="flex-container">
-        <!-- <div class="red" style="width:567px;">test</div>
-        <div class="red" style="width:567px;">test</div> -->
-    
+            <!-- <div class="red" style="width:567px;">test</div>
+            <div class="red" style="width:567px;">test</div> -->
+
             <div class="carousel-wrapper z-depth-2">
-            <div class="carousel carousel-slider center" ref="carousel" id="carouselExtrait">
-                
-            <!-- <div class="carousel-fixed-item center">
-                    <a class="btn waves-effect white grey-text darken-text-2">button</a>
-                </div> -->
-                <div class="carousel-item white-text" href="#one!">
-                    <!-- <h4>First Panel</h4> -->
-                    <img class="responsive-img" alt="Le déradicaliseur image BD" src="@/assets/planche_45_ST.jpg">
-                    <p class="white-text">{{ items.item1 }}</p>
-                </div>
-                <div class="carousel-item white-text" href="#two!">
-                    <!-- <h4>Second Panel</h4> -->
-                    <img class="responsive-img" alt="Le déradicaliseur image BD" src="@/assets/planche_24_ST.jpg">
-                    <p class="white-text">{{ items.item2 }}</p>
-                </div>
-                <!--  <div class="carousel-item green white-text" href="#three!">
-                    <h2>Third Panel</h2>
+                <div class="carousel carousel-slider center" ref="carousel" id="carouselExtrait">
+
+                    <!-- <div class="carousel-fixed-item center">
+                        <a class="btn waves-effect white grey-text darken-text-2">button</a>
+                    </div> -->
+                    <div class="carousel-item white-text">
+                        <!-- <h4>First Panel</h4> -->
+                        <img class="responsive-img" alt="Le déradicaliseur image BD" src="@/assets/planche_45_ST.jpg">
+                        <p class="white-text">{{ items.item1 }}</p>
+                    </div>
+                    <div class="carousel-item white-text">
+                        <!-- <h4>Second Panel</h4> -->
+                        <img class="responsive-img" alt="Le déradicaliseur image BD" src="@/assets/planche_24_ST.jpg">
+                        <p class="white-text">{{ items.item2 }}</p>
+                    </div>
+                    <!--  <div class="carousel-item green white-text" href="#three!">
+                        <h2>Third Panel</h2>
                     <p class="white-text">This is your third panel</p>
+                    </div>
+                    <div class="carousel-item blue white-text" href="#four!">
+                        <h2>Fourth Panel</h2>
+                        <p class="white-text">This is your fourth panel</p>
+                    </div> -->
                 </div>
-                <div class="carousel-item blue white-text" href="#four!">
-                    <h2>Fourth Panel</h2>
-                    <p class="white-text">This is your fourth panel</p>
-                </div> -->
             </div>
-        </div>
-        <p>|||||||||||||</p>
+            <p>|||||||||||||</p>
         </div>
     </div>
 </template>
 <script>
-  import M from 'materialize-css'
-  export default {
-    name: 'CarouselExtraits',
-    mounted () {    
-        new M.Carousel(this.$refs.carousel, {
-            indicators: true,
-            fullWidth: true,
-            noWrap: true
-        })
-    },
-    methods:{
-        
-    },
-    data () {
-      return {
-          title: 'Ceci est un slider',
-          items: {
-             item1: "(extrait planche 45)",
-             item2: "(planche 24)", 
-          }
-      }
+    import M from 'materialize-css'
+    export default {
+        name: 'CarouselExtraits',
+        mounted() {
+            new M.Carousel(this.$refs.carousel, {
+                indicators: true,
+                fullWidth: true,
+                noWrap: true
+            })
+        },
+        methods: {
+
+        },
+        data() {
+            return {
+                title: 'Ceci est un slider',
+                items: {
+                    item1: "(extrait planche 45)",
+                    item2: "(planche 24)",
+                }
+            }
+        }
     }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.flex-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center; 
-}
-
-.carousel p{
-    padding: 0px;
-    margin: 0px;
-    color:#333 !important;
-}
-
-img{
-    max-width: 100%;
-}
-
-/* Mobile Devices <= 600px */
-
-@media only screen and (max-width: 600px) {
-    p{
-        color:red;
+    .flex-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 
-    #carouselExtrait {
-        height: 560px !important;
-        width:350px !important;
-    }
-}
-
-/* Tablet Devices > 600px & <= 992px */
-
-@media only screen and (min-width: 601px) and (max-width: 992px) {
-    p{
-        color:blue;
-    }
-    #carouselExtrait {
-        height: 850px !important;
-        width:567px !important;
-    }
-}
-
-/* Desktop Devices > 992 & <= 1200px */
-
-@media only screen and (min-width: 993px) and (max-width: 1200px) {
-    
-    p{
-        color:purple;
+    .carousel {
+        cursor: pointer;
     }
 
-    #carouselExtrait {
-        height: 850px !important;
-        width:567px !important;
+    .carousel p {
+        padding: 0px;
+        margin: 0px;
+        color: #333 !important;
     }
-}
 
-/* Large Desktop Devices  > 1200px */
-
-@media only screen and (min-width: 1201px) {
-
-    p{
-        color:greenyellow;
+    img {
+        max-width: 100%;
     }
-    #carouselExtrait {
-        height: 850px !important;
-        width:567px !important;
+
+    /* Mobile Devices <= 600px */
+
+    @media only screen and (max-width: 600px) {
+        p {
+            color: red;
+        }
+
+        #carouselExtrait {
+            height: 560px !important;
+            width: 350px !important;
+        }
     }
-}
 
+    /* Tablet Devices > 600px & <= 992px */
 
+    @media only screen and (min-width: 601px) and (max-width: 992px) {
+        p {
+            color: blue;
+        }
+
+        #carouselExtrait {
+            height: 850px !important;
+            width: 567px !important;
+        }
+    }
+
+    /* Desktop Devices > 992 & <= 1200px */
+
+    @media only screen and (min-width: 993px) and (max-width: 1200px) {
+
+        p {
+            color: purple;
+        }
+
+        #carouselExtrait {
+            height: 850px !important;
+            width: 567px !important;
+        }
+    }
+
+    /* Large Desktop Devices  > 1200px */
+
+    @media only screen and (min-width: 1201px) {
+
+        p {
+            color: greenyellow;
+        }
+
+        #carouselExtrait {
+            height: 850px !important;
+            width: 567px !important;
+        }
+    }
 </style>
 <style>
-/* Not in scope */
+    /* Not in scope */
 
-#carouselExtrait .indicators .indicator-item {
-    background-color: grey;
-}
+    #carouselExtrait .indicators .indicator-item {
+        background-color: grey;
+    }
 
-#carouselExtrait .indicators .indicator-item.active {
-    background-color: yellow;
-}
+    #carouselExtrait .indicators .indicator-item.active {
+        background-color: yellow;
+    }
 </style>
