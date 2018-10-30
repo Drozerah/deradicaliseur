6a4d13e1a7d2d57a7e2282c4ec176c8a13b399bf
Drozerah <drozerah@drozerah.user>
2018-10-30 19:21:46 +0100

    feat: add link to /entrevue + conditional responsive divider

--- src/components/tools/CarouselExtraits.vue
+++ src/components/tools/CarouselExtraits.vue
@@ -45,12 +45,28 @@
                 Est-ce un oubli ?<br>
                 Est-ce par "fourberie" ? Oui et Non.</p>
 
-                <p>Nous avons choisi de ne pas mettre le texte parce qu'il serait forcément sorti de son contexte, et il serait difficile de saisir la progression, la stratégie du déradicaliseur.
+                <p>
+                Nous avons choisi de ne pas mettre le texte parce qu'il serait forcément sorti de son contexte, et il serait difficile de saisir la progression, la stratégie du déradicaliseur.
                 Pour voir le texte correspondant à chaque image ci-dessous, il faudra donc aller chez votre libraire favori, et au pire, le commander sans savoir de quoi il en retourne. Mais rassurez vous, nous avons travaillé avec beaucoup d'application pour que vous ne regrettiez pas d'avoir cette BD entre les mains. 
                 </p>
 
-                <p>Comme pour la précédente BD "Joanne Lebster, le début d'un nouveau monde", réalisée par Mathieu et Marc, préparez-vous à aller où peu de BDs (de part leur theme) vous ont déjà emmené.
-                :)</p>
+                <p>
+                Comme pour la précédente BD "Joanne Lebster, le début d'un nouveau monde", réalisée par Mathieu et Marc, préparez-vous à aller où peu de BDs (de part leur theme) vous ont déjà emmené.
+                :)
+                </p>
+                <!-- DESKTOP only -->
+                <b v-if="$mq == 'desktop'">
+                    <br>
+                    <div class="divider"></div>
+                    <br>
+                </b>
+
+                <p> 
+                voir aussi :   
+                </p>    
+                <div class="center">
+                    <router-link to="entrevue" class="btn waves-effect waves-yellow grey lighten-1 black-text darken-text-2">Interview présentation de l'ouvrage</router-link>
+                </div>                
             </div>
         </div>
     </div>
@@ -129,6 +145,9 @@
 
     .main-content .text-content p .bold {
         font-weight: 700;
+    }
+    .main-content .text-content .btn {
+        text-transform: none;
     }
 
 /* RESPONSIVE RULES */
