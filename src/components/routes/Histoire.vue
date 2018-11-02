62bb79a19944ac3a5a7380df19e0b5bb717cb1d8
Drozerah <drozerah@drozerah.user>
2018-11-02 17:36:13 +0100

    feat: add 'Histoire' content + CSS + img + link

--- src/components/routes/Histoire.vue
+++ src/components/routes/Histoire.vue
@@ -1,8 +1,40 @@
 <template>
-  <article class="histoire">
+  <article 
+    class="histoire" 
+    :class="$mq"
+  >
     <header class="article-header">
       <h1 class="article-title">{{ title }}</h1>
     </header>
+    <div class="content flex-container">
+      <p class="text-content flex-item">
+          Après un entretien avec des responsables du département de la justice, un déradicaliseur est autorisé à communiquer avec un détenu.<br> 
+          Ils vont ainsi parler de presque tout, sans tabou,<br>
+          quitte à remettre en question l'impensable...
+      </p>
+      <br>
+      <div class="z-depth-2 img-card flex-item">
+        <img class="responsive-img"  src="@/assets/planche_27_ST.jpg">
+        <div class="center">- extrait planche 27 -</div>
+      </div>
+      <div class="flex-item">
+      <br>
+      <br>
+      <div class="divider"></div>
+      <br>
+      <p>
+      Plusieurs extraits sont disponibles en suivant le lien ci-dessous :
+      </p>
+      <br>
+      <div class="center">
+          <router-link to="extraits" class="btn waves-effect waves-yellow grey lighten-1 black-text darken-text-2">Extraits</router-link>
+      </div>
+      </div>
+      <br>
+
+    </div>
+
+
   </article>
 </template>
 
@@ -11,7 +43,7 @@ export default {
   name: 'Histoire',
   data(){
     return{
-      title: "Histoire component title"
+      title: "L'histoire"
     }
   }
 }
@@ -19,18 +51,35 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+/* FlexBox */
+
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.flex-item {
+  color:#333;
+  width: 567px;
+  padding: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.text-content {
+  margin-top: 0px;
 }
-a {
-  color: #42b983;
+
+.mobile .flex-item{
+  width: 350px;
+}
+
+.img-card{
+  /* padding: 10px 10px 20px 10px; */
+  padding-bottom: 15px;
+}
+
+.content .btn {
+  text-transform: none;
 }
 </style>
