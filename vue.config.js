a17cff38f8de1663ccdefbab078301bc8ccc56bf
Drozerah <drozerah@drozerah.user>
2018-11-07 20:12:35 +0100

    feat: add vue.config.js file

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  chainWebpack: config => { 
+    // To find out the plugin names to delete: vue inspect --plugins.
+    // config.plugins.delete('preload')
+    // config.plugins.delete('prefetch')
+  }
+}
+
