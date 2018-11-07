a43489c4d0e4c2e0fc2984dbde127a043c202ab7
Drozerah <drozerah@drozerah.user>
2018-11-07 20:34:07 +0100

    feat: disable preload plugin

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   chainWebpack: config => { 
     // To find out the plugin names to delete: vue inspect --plugins.
-    // config.plugins.delete('preload')
+    config.plugins.delete('preload')
     // config.plugins.delete('prefetch')
   }
 }
